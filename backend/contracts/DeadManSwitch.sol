eec6fdcaef8a27a8b5038ea2e4af50c12a5f53eb
123Miki <maxence.dugain@gmail.com>
2026-04-23 22:30:38 +0200

    config

--- backend/contracts/DeadManSwitch.sol
+++ backend/contracts/DeadManSwitch.sol
@@ -2,6 +2,14 @@
 pragma solidity 0.8.28;
 
 contract DeadManSwitch {
+    //-----------------------------------
+    //-----STORAGE-----------------------
+    //-----------------------------------
+    address public owner;
+    uint96 public lastPing;
+    address payable public heir;
+    uint96 public inactivityDelay;
+
 
 
 }
